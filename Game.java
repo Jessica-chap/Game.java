8cabfa75d7f25302241ee2c60d6030186d2ef829
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-06-01 19:58:48 +0000

    Finished basic lab, and some further study on multi games

--- Game.java
+++ Game.java
@@ -6,13 +6,10 @@ import java.util.Random;
 A number-guessing game.
 
 */ 
-//need to get random number from 1-100
-//while loop 
-//need to ask user for their guess and get back their input
-//conditionals to handle their guess
-//tracker to keep track of the number of guesses
-//if too high, or too low relay message letting the user know
-//if correct number found number and how many guesses 
+//declare class Main
+
+//can call the class Game newGame = new Game()
+
 
 public class Game {
   public static void main(String[] args){
@@ -20,24 +17,84 @@ public class Game {
     System.out.println("Hello what is your name?");
     String name = input.nextLine();
 
-    System.out.println("Nice to meet you " + name +", guess a number 1-100");
+    System.out.println("Nice to meet you " + name );
     
-    Random rand = new Random();
-    int number = rand.nextInt(100);
+    Boolean playAgain = true;
+    int rounds = 0;
+    int bestGuesses = 100000;
 
-    int guessNumber = input.nextInt();
+//main game
+    while (playAgain == true){
 
-    while (guessNumber != number){
+      Random rand = new Random();
+      int number = rand.nextInt(100);
 
-      if (guessNumber == number);
-        System.out.println("Correct guess, you win!");
-        break;
-      if (guessNumber >  number );
-        System.out.println("Your guess is too high, try again.");
+      System.out.println(number);
+
+      System.out.println("Guess a number 1-100");
+      int guessNumber = input.nextInt();
+
+      int guessTracker = 0;
+
+        
+      while (true) {
+
+        rounds = rounds + 1;
+
+        if (guessNumber > 100  | guessNumber < 1){
+          System.out.println("Guess within range 1-100");
+          guessNumber = input.nextInt();
+        }
+
+        else if (guessNumber >  number ){
+          System.out.println("Your guess is too high, try again.");
+          guessTracker += 1;
+          System.out.println("Guess a number 1-100");
+          guessNumber = input.nextInt();
+          }
+          
+        else if (guessNumber <  number ){
+          System.out.println("Your guess is too low, try again.");
+          guessTracker += 1;
+          System.out.println("Guess a number 1-100");
+          guessNumber = input.nextInt();}
+
+        else if (guessNumber == number){
+          guessTracker += 1;
+          System.out.println("Correct guess, you win! Only took "+ guessTracker + " tries!");
+          if (guessTracker < bestGuesses){
+            bestGuesses = guessTracker;
+            System.out.println("Your best score is now " + bestGuesses);
+          }
+          else {
+            System.out.println("Your number of guesses was not less than your best guess of " + bestGuesses);
+          }
+          break;
+  
+          // playAgain.equals(newGame);
+            // break;
+        
 
 
       }
+      System.out.println("Would you like to play again Yes or No?");
+      String newGame = input.next();
+      if (newGame.equals("No")){
+        playAgain = false;
+        System.out.println("Thanks for playing " + rounds + " rounds");
+        // break;
+      }
+      //  else{
+      //   rounds += 1;
+      //   System.out.println(rounds);
+      // }
+          
+
+    }
     
+  }
+  }
 }
-}
+
+
 
